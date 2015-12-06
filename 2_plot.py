8e5a14d22f7f26f4d7cf27d400e99597c79c97ec
Rony Gregory <rony.gregory@utah.edu>
2015-12-06 15:27:16 -0700

    Added GUI.

--- 2_plot.py
+++ 2_plot.py
@@ -2,16 +2,22 @@ import sys
 from pyspark import SparkConf, SparkContext
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
+import gui
 
-#Filter criteria - change it to accept from UI
+# Filter criteria - change it to accept from UI
 print "starting"
-country="Hungary"
-indicator="GINI index"
-year_start = 1971
-year_end = 2015
+gui.getValues()
+country = gui.country
+print country
+indicator = gui.indicator
+print indicator
+year_start = gui.startYear
+print year_start
+year_end = gui.endYear
+print  year_end
 num_of_segments = 5
 
-predict_end=2015	#predict till year
+predict_end = gui.predYear  # predict till year
 
 
 
@@ -21,93 +27,97 @@ conf.setAppName("damu1000")
 conf.set("spark.executor.memory", "4g")
 sc = SparkContext(conf=conf)
 
-#read data
+# read data
 lines = sc.pickleFile(".//result")
-#filter by country, indicator and period. sort by period
-lines = lines.filter(lambda x: x[0]==country and x[2]==indicator and x[4] != '' and x[3] >= year_start and x[3] <= year_end).sortBy(lambda (a,b,c,d,e): d, True).cache()
+# filter by country, indicator and period. sort by period
+lines = lines.filter(lambda x: x[0] == country and x[2] == indicator and x[4] != '' and x[3] >= year_start and x[3] <= year_end).sortBy(lambda (a, b, c, d, e): d, True).cache()
+lines.take(1)
+if not lines.take(1):
+    print "Index not present for this country. Stopping"
+    sys.exit()
 print lines.collect()
-x = lines.map(lambda (a,b,c,d,e): (d)  )	#getting x values in 2D RDD
-y = lines.map(lambda (a,b,c,d,e): float(e)  )	#getting x values in 2D RDD
+x = lines.map(lambda (a, b, c, d, e): (d))  # getting x values in 2D RDD
+y = lines.map(lambda (a, b, c, d, e): float(e))  # getting x values in 2D RDD
 
 
-#num_of_segments = x.count() / 5 #averaging at around 5 points per segment
+# num_of_segments = x.count() / 5 #averaging at around 5 points per segment
 #--------------------------------------- Find out "break" points in pattern--------------------------------------------------------------------
 
-#assign indexes to y values, increment by 1 and 2 so that elements can be joined to find out diff later.
-x0 = x.zipWithIndex().map(lambda (a,b): (b,a))
-y0 = y.zipWithIndex().map(lambda (a,b): (b,a))
-y1 = y0.map(lambda (a,b): (a+1,b) )
-y2 = y0.map(lambda (a,b): (a+2,b) )
+# assign indexes to y values, increment by 1 and 2 so that elements can be joined to find out diff later.
+x0 = x.zipWithIndex().map(lambda (a, b): (b, a))
+y0 = y.zipWithIndex().map(lambda (a, b): (b, a))
+y1 = y0.map(lambda (a, b): (a + 1, b))
+y2 = y0.map(lambda (a, b): (a + 2, b))
 # join structure is like: [index, ((y0, y1) ,y2 )]. Hence the structure used in next map
-#y[1] -> y[i+2], y[0][1] -> y[i+1], y[0][0] -> y[i]. Calculating diff (y[i+2]-y[i+1]) - (y[i+1]-y[i]) to find out breakpoint
-#finding out difference in y coordinates of every consecutive pair. sort by desc order of difference
-y_join = y0.join(y1).join(y2).map(lambda (x,y): (x, abs( (y[1]-y[0][1]) - (y[0][1] - y[0][0]) )) ).sortBy(lambda (a,b): b, False )
+# y[1] -> y[i+2], y[0][1] -> y[i+1], y[0][0] -> y[i]. Calculating diff (y[i+2]-y[i+1]) - (y[i+1]-y[i]) to find out breakpoint
+# finding out difference in y coordinates of every consecutive pair. sort by desc order of difference
+y_join = y0.join(y1).join(y2).map(lambda (x, y): (x, abs((y[1] - y[0][1]) - (y[0][1] - y[0][0])))).sortBy(lambda (a, b): b, False)
 
-#caution: segments are derived from zip index. hence contain index of element NOT x element (i.e. year)
-#picking up top difference elements - they form our segments
-segments = sorted(y_join.map(lambda (a,b):a).take(num_of_segments))	#taking 2 additional segments. Because some times diff returns 2 consecutive points, which has to be discarded
+# caution: segments are derived from zip index. hence contain index of element NOT x element (i.e. year)
+# picking up top difference elements - they form our segments
+segments = sorted(y_join.map(lambda (a, b):a).take(num_of_segments))  # taking 2 additional segments. Because some times diff returns 2 consecutive points, which has to be discarded
 segments = [0] + segments
 segments.append(x.count())
 print segments
-i=0
-#remove consecutive points if included
-#check for divide by 0. some prob in segmentation. print x1, check segment should have more than one element.
-while i<len(segments)-1:
-        if segments[i]+1==segments[i+1] or segments[i]+2==segments[i+1]:
+i = 0
+# remove consecutive points if included
+# check for divide by 0. some prob in segmentation. print x1, check segment should have more than one element.
+while i < len(segments) - 1:
+        if segments[i] + 1 == segments[i + 1] or segments[i] + 2 == segments[i + 1]:
                 segments.remove(segments[i])
 	else:
-	        i=i+1	#increment i only in else, cause if element is deleted, then next element is now at same old i th position
-if segments[0]!=0:
+	        i = i + 1  # increment i only in else, cause if element is deleted, then next element is now at same old i th position
+if segments[0] != 0:
 	segments = [0] + segments
 
 print segments
 
 #------------------------------------------------Calculate linear regression for every segment---------------------------------------------
-seg=0
+seg = 0
 y_est = []
 y = y.collect()
 
-#for carry out linear regression for individual segment
-while seg < len(segments)-1:
+# for carry out linear regression for individual segment
+while seg < len(segments) - 1:
 	print "----------------------------------------------------------------------------------------------------"
 	start = segments[seg]
-	end = segments[seg+1]
-        #B1 = sum( (y_avg - yi)(x_avg - xi) ) / sum ( (x_avg - xi)(x_avg - xi) )
-        #B0 = y_avg - B1*x_avg
-	x1 = x0.filter(lambda (a,b): a >= start and a < end).map(lambda (a,b):float(b))	#filter x and y for current segment start and end
-	y1 = y0.filter(lambda (a,b): a >= start and a < end).map(lambda (a,b):float(b))
+	end = segments[seg + 1]
+        # B1 = sum( (y_avg - yi)(x_avg - xi) ) / sum ( (x_avg - xi)(x_avg - xi) )
+        # B0 = y_avg - B1*x_avg
+	x1 = x0.filter(lambda (a, b): a >= start and a < end).map(lambda (a, b):float(b))  # filter x and y for current segment start and end
+	y1 = y0.filter(lambda (a, b): a >= start and a < end).map(lambda (a, b):float(b))
 	seg = seg + 1
 
-        x_avg = float(x1.sum())/x1.count()	#find out average
-        y_avg = float(y1.sum())/y1.count()
-        i=0
-        num=0
-        den=0
+        x_avg = float(x1.sum()) / x1.count()  # find out average
+        y_avg = float(y1.sum()) / y1.count()
+        i = 0
+        num = 0
+        den = 0
 	
 	print "x1: ", x1.collect()
 	print "y1: ", y1.collect()
 	
-	num = x1.zip(y1).map(lambda (a,b): (y_avg - b) * (x_avg - a)).sum()	#numerator and denominator as per above formula of linear regression
-	den = den + x1.map(lambda a: (x_avg - a) * (x_avg - a) ).sum()
+	num = x1.zip(y1).map(lambda (a, b): (y_avg - b) * (x_avg - a)).sum()  # numerator and denominator as per above formula of linear regression
+	den = den + x1.map(lambda a: (x_avg - a) * (x_avg - a)).sum()
 
-        B1 = num / den		#B0 and B1 (Beta 0 and Beta 1) calculation
-        B0 = y_avg - B1*x_avg
+        B1 = num / den  # B0 and B1 (Beta 0 and Beta 1) calculation
+        B0 = y_avg - B1 * x_avg
 
-	#y1 = y1.collect()
-	y_est = y_est + x1.map(lambda a: B0 + B1*a).collect()	#calculate estimated linear regression using Beta0 and Beta 1 (finally!!!)
+	# y1 = y1.collect()
+	y_est = y_est + x1.map(lambda a: B0 + B1 * a).collect()  # calculate estimated linear regression using Beta0 and Beta 1 (finally!!!)
 
 #---------------------------------------extend x with upcoming years and predict using last segment's B0 and B1
 print "estimating--------------------------------------------------------------------------------------"
-x=x.collect()
-predict_start = x[len(x)-1] + 1		#first year of prediction = last year of available data + 1
+x = x.collect()
+predict_start = x[len(x) - 1] + 1  # first year of prediction = last year of available data + 1
 
-x_est = x + range(predict_start, predict_end+1)	#adding + 1 to accomodate end year as well
+x_est = x + range(predict_start, predict_end + 1)  # adding + 1 to accomodate end year as well
 
-for i in range(predict_start, predict_end+1):
-	y_est.append(B0 + B1*i)
+for i in range(predict_start, predict_end + 1):
+	y_est.append(B0 + B1 * i)
 
-#plt.plot(x,y,'bs', x_est, y_est, 'g--')
-#plt.show()
+# plt.plot(x,y,'bs', x_est, y_est, 'g--')
+# plt.show()
 print "plotting"
 print "x_est: ", x_est
 print len(x_est)
@@ -117,10 +127,10 @@ print len(y_est)
 plt.xlabel('Year')
 plt.ylabel(indicator)
 
-line1, = plt.plot(x_est,y_est,linestyle="solid", marker='o', color="blue")
-line2, = plt.plot(x,y, linestyle="s", marker="o", color="red")
+line1, = plt.plot(x_est, y_est, linestyle="solid", marker='o', color="blue")
+line2, = plt.plot(x, y, linestyle="s", marker="o", color="red")
 
-plt.legend([line1,line2], ["Prediction","Real Data"])
+plt.legend([line1, line2], ["Prediction", "Real Data"])
 plt.title("Prediction using Segmented Linear Regression: " + country)
 plt.show()
 
--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import wx
 
 class ComboBoxFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, -1, 'Select Your Data:', size=(375, 300))
+        wx.Frame.__init__(self, None, -1, 'Select Your Data:', size=(600, 300))
         panel = wx.Panel(self, -1)
         
         diff = 30
@@ -12,19 +12,20 @@ class ComboBoxFrame(wx.Frame):
         fieldX = 170
         fieldY = 10
 
-        countryList = ['India', 'Pakistan', 'USA']
+        countryList = ['Hungary','United States', 'Brazil', 'United Kingdom', 'Uganda', 'Australia', 'India', 'Russian Federation']
         wx.StaticText(panel, -1, "Country:", (textX, textY))
         self.countryCombo = wx.ComboBox(panel, -1, countryList[0], (fieldX, fieldY), wx.DefaultSize, countryList, wx.CB_DROPDOWN)
         
         textY += 30
         fieldY += 30
-        indicatorList = ['indicator1', 'indicator2', 'indicator3']
+        indicatorList = ['GINI index', 'Achieve universal primary education',\
+                          'GDP, PPP (constant 2011 international $)','Poverty gap at national poverty line (%)']
         wx.StaticText(panel, -1, "Indicator:", (textX, textY))
-        self.indicatorCombo = wx.ComboBox(panel, -1, indicatorList[0], (fieldX, fieldY), wx.DefaultSize, indicatorList, wx.CB_DROPDOWN)
+        self.indicatorCombo = wx.ComboBox(panel, -1, indicatorList[0], (fieldX, fieldY), (300,-1), indicatorList, wx.CB_DROPDOWN)
 
         textY += 30
         fieldY += 30
-        yearList = range(1970, 2016)  # 0 to 2015
+        yearList = range(1972, 2008)  # 0 to 2007
         yearList = map(str, yearList)
         wx.StaticText(panel, -1, "Start year:", (textX, textY))
         self.startYearCombo = wx.ComboBox(panel, -1, yearList[0], (fieldX, fieldY), wx.DefaultSize, yearList, wx.CB_DROPDOWN)
@@ -39,24 +40,31 @@ class ComboBoxFrame(wx.Frame):
         wx.StaticText(panel, -1, "Prediction end year:", pos=wx.Point(textX, textY))
         self.predEndYearText = wx.TextCtrl(panel, -1, value="2015", pos=wx.Point(fieldX, fieldY), size=wx.Size(175, 25))
                         
-        self.button1 = wx.Button(panel, -1, label="Calculate", pos=wx.Point(fieldX, 160), size=wx.Size(175, 28))
+        self.button1 = wx.Button(panel, -1, label="Save and Exit", pos=wx.Point(fieldX, 160), size=wx.Size(175, 28))
         self.button1.Bind(wx.EVT_BUTTON, self.button1Click, self.button1)
 
     def button1Click(self, event):
         """Conversion button has been clicked"""
+        global country 
         country = self.countryCombo.GetValue()
+        global indicator
         indicator = self.indicatorCombo.GetValue()
-      	startYear = self.startYearCombo.GetValue()
-        endYear = self.endYearCombo.GetValue()
-        predYear = self.predEndYearText.GetValue()
+      	global startYear
+        startYear = int(self.startYearCombo.GetValue())
+        global endYear
+        endYear = int(self.endYearCombo.GetValue())
+        global predYear
+        predYear = int(self.predEndYearText.GetValue())
 
         str = "country: %s, indicator: %s, startYear: %s, endYear: %s, predYear: %s" % (country, indicator, startYear, endYear, predYear)
         print str
+        self.Close()
 
 def getValues():
     app = wx.PySimpleApp()
     ComboBoxFrame().Show()
     app.MainLoop()
+    str = "country: %s, indicator: %s, startYear: %s, endYear: %s, predYear: %s" % (country, indicator, startYear, endYear, predYear)
 
 if __name__ == "__main__":    
     getValues()
